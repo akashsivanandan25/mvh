17229f3bb7b0df0c12726dd9f33d33afb6b7cf4f
akash <akash.sivanandan@pricelabs.co>
2025-11-26 17:17:26 -0500

    Add utility class containing constants

---
+++ src/utils/Gameconstants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Gameconstants {
+    public static final float ENCOUNTER_CHANCE = 0.35f;
+    public static final float REGEN_PERCENT = 0.10f;
+    public static final float SELL_PRICE_RATIO = 0.5f;
+    public static final int WORLD_HEIGHT = 8;
+    public static final int WORLD_WIDTH = 8;
+}
