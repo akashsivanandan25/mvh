69db26cb67ed2927bb756eff0ce5911c0467daa8
akash <akash.sivanandan@pricelabs.co>
2025-11-26 17:31:54 -0500

    Create an abstrat class to represent ingame items

---
+++ src/item/Item.java
@@ -0,0 +1,33 @@
+package item;
+
+public abstract class Item {
+    private final String name;
+    private final int price;
+    private final int levelRequired;
+    private final int maxUses;
+    private int currentUses;
+
+    public Item(String name, int price, int levelRequired, int maxUses) {
+        this.name = name;
+        this.price = price;
+        this.levelRequired = levelRequired;
+        this.maxUses = maxUses;
+        this.currentUses = 0;
+    }
+
+    public void use(){
+        this.currentUses++;
+    }
+
+    public boolean isUsable(){
+        return this.currentUses < this.maxUses;
+    }
+
+    public void repair(){
+        this.currentUses = 0;
+    }
+
+    public int getSellingPrice(){
+        return this.price / 2;
+    }
+}
