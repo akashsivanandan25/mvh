0aa97b0f3967d7f64ae0d23b838fd08713b46f4b
akash <akash.sivanandan@pricelabs.co>
2025-11-26 18:08:20 -0500

    Create a basic inventory

---
+++ src/inventory/Inventory.java
@@ -0,0 +1,77 @@
+package inventory;
+import java.util.*;
+import item.*;
+
+public class Inventory {
+    private List<Item> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public void add(Item item) {
+        this.items.add(item);
+    }
+
+    public void remove(Item item) {
+        this.items.remove(item);
+    }
+
+    public List<Weapon> getWeapons(){
+        List<Weapon> toReturn = new ArrayList<>();
+        for(Item item : items){
+            if (item instanceof Weapon){
+                toReturn.add((Weapon) item);
+            }
+        }
+        return toReturn;
+    }
+
+    public List<Armour> getArmours(){
+        List<Armour> toReturn = new ArrayList<>();
+        for (Item item : items){
+            if (item instanceof Armour){
+                toReturn.add((Armour) item);
+            }
+        }
+        return toReturn;
+    }
+
+    public List<Spell> getSpells(){
+        List<Spell> toReturn = new ArrayList<>();
+        for (Item item : items){
+            if (item instanceof Spell){
+                toReturn.add((Spell) item);
+            }
+        }
+        return toReturn;
+    }
+
+
+    public List<Potion> getPotions(){
+        List<Potion> toReturn = new ArrayList<>();
+        for (Item item : items){
+            if (item instanceof Potion){
+                toReturn.add((Potion) item);
+            }
+        }
+        return toReturn;
+    }
+
+    public Item getItemByName(String name){
+        for(Item item : items){
+            if (item.getName().equals(name)){
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return items.isEmpty();
+    }
+
+    public List<Item> getInventory(){
+        return this.items;
+    }
+}
