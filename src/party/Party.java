41452126ab2276c8d7ea292f95828bfced793b94
akash <akash.sivanandan@pricelabs.co>
2025-11-26 22:28:36 -0500

    Add basic party implementation

---
+++ src/party/Party.java
@@ -0,0 +1,54 @@
+package party;
+import character.*;
+import world.Position;
+
+import java.util.*;
+
+public class Party {
+    private List<Hero> heroes;
+    private Position position;
+
+
+    public Party(List<Hero> heroes){
+        this.heroes = heroes;
+        this.position = new Position(0,0);
+    }
+
+    public List<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public boolean allFainted(){
+        int count = 0;
+        for (Hero hero : heroes){
+            if(hero.isFainted()){
+                count ++;
+            }
+        }
+
+        if (count == heroes.size()){
+            return true;
+        }
+        return false;
+    }
+
+
+    public void reviveAll(){
+        for (Hero hero : heroes){
+            if (hero.isFainted()){
+                hero.heal(50);
+            }
+        }
+    }
+
+
+    public int getHighestLevel(){
+        int maxLevel = -1;
+        for  (Hero hero : heroes){
+            if (hero.getLevel() > maxLevel){
+                maxLevel = hero.getLevel();
+            }
+        }
+        return maxLevel;
+    }
+}
