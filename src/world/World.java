b31021e3614e8e47cb1abbd209ba42648991e177
akash <akash.sivanandan@pricelabs.co>
2025-11-26 23:21:23 -0500

    Add initial world implementation

---
+++ src/world/World.java
@@ -0,0 +1,52 @@
+package world;
+
+import utils.Gameconstants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class World {
+    private Tile[][] tiles;
+    public Position partyPosition;
+
+    public World(){
+        generateTiles();
+        placePartyStarting();
+    }
+
+    private void generateTiles() {
+        int total = Gameconstants.WORLD_HEIGHT * Gameconstants.WORLD_WIDTH;
+        int inaccessibleCount = (int) (total * 0.2f);
+        int marketCount    = (int) (total * 0.3f);
+        int commonCount =  total -  inaccessibleCount - marketCount;
+
+        List<Tile> pool = new ArrayList<Tile>(total);
+        for (int i = 0; i < inaccessibleCount; i ++) pool.add(new InaccessibleTile());
+        for (int i = 0; i < marketCount; i ++) pool.add(new MarketTile());
+        for (int i = 0; i < commonCount; i ++) pool.add(new CommonTile());
+
+        java.util.Collections.shuffle(pool);
+
+        int k = 0;
+        for (int r = 0; r < Gameconstants.WORLD_HEIGHT; r ++){
+            for (int c = 0; c < Gameconstants.WORLD_WIDTH; c ++){
+                tiles[r][c] = pool.get(k++);
+            }
+        }
+    }
+
+    private void placePartyStarting(){
+        for (int r = 0; r < Gameconstants.WORLD_HEIGHT; r ++){
+            for (int c = 0; c < Gameconstants.WORLD_WIDTH; c ++){
+                if (tiles[r][c].isAccessible()){
+                    this.partyPosition = new Position(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    public Tile tileAtPos(Position pos){
+        return tiles[pos.getX()][pos.getY()];
+    }
+}
