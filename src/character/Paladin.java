f7faa6dfba6b5ca32c249c3f4bbbb760f58b9cac
akash <akash.sivanandan@pricelabs.co>
2025-11-26 22:26:18 -0500

    Add paladin implementation

--- src/character/Paladin.java
+++ src/character/Paladin.java
@@ -1,16 +1,27 @@
 package character;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Paladin extends Hero {
+    private final List<StatType> favouredStats;
+    protected Paladin(String name, int level, int maxHP, int maxMp, int strength, int dex, int agility) {
+        super(name, level, maxHP, maxMp, strength, dex, agility);
+        this.favouredStats = new ArrayList<>();
+        favouredStats.add(StatType.STRENGTH);
+        favouredStats.add(StatType.DEX);
+    }
+
     @Override
     protected void applyLevelUpBonuses() {
-
+        this.buffDex(10);
+        this.buffStrength(10);
+        this.buffAgility(5);
     }
 
     @Override
     public List<StatType> getFavouredStats() {
-        return Collections.emptyList();
+        return Collections.unmodifiableList(favouredStats);
     }
 }
