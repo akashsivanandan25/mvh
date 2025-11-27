a1b6a64f786cb3905ed7bbae0755751468cecdbb
akash <akash.sivanandan@pricelabs.co>
2025-11-26 21:46:12 -0500

    Implement apply function for Potion

--- src/item/Potion.java
+++ src/item/Potion.java
@@ -1,19 +1,39 @@
 package item;
 
+import character.Hero;
 import character.StatType;
 
 public class Potion extends Item{
     private final StatType affectedStat;
-    private final float effectAmount;
+    private final int effectAmount;
 
-    public Potion(String name, int price, int levelRequired, int maxUses, StatType affectedStat, float effectAmount) {
+    public Potion(String name, int price, int levelRequired, int maxUses, StatType affectedStat, int effectAmount) {
         super(name, price, levelRequired, maxUses);
         this.affectedStat = affectedStat;
         this.effectAmount = effectAmount;
     }
 
 
-    public void apply(){
-        // TODO: Pass a character object here
+    public void apply(Hero character){
+        switch (affectedStat){
+            case AGILITY:
+                character.buffAgility(effectAmount);
+                break;
+            case DEX:
+                character.buffDex(effectAmount);
+                break;
+            case STRENGTH:
+                character.buffStrength(effectAmount);
+                break;
+            case HP:
+                character.heal(effectAmount);
+                break;
+            case MP:
+                character.buffMP(effectAmount);
+                break;
+            default:
+                break;
+        }
+        super.use();
     }
 }
