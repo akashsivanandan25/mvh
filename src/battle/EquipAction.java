ce4083bcd97816def30b254d95915b5bdae00c9c
akash <akash.sivanandan@pricelabs.co>
2025-11-29 16:43:58 -0500

    Add EquipAction to centralise equipping logic

---
+++ src/battle/EquipAction.java
@@ -0,0 +1,26 @@
+package battle;
+
+import character.Hero;
+import item.Equipment;
+
+public class EquipAction implements BattleAction {
+
+    private final Hero hero;
+    private final Equipment equipment;
+
+    public EquipAction(Hero hero, Equipment equipment) {
+        this.hero = hero;
+        this.equipment = equipment;
+    }
+
+    @Override
+    public void execute(Battle battle) {
+        hero.equip(equipment);
+        battle.log(getDescription());
+    }
+
+    @Override
+    public String getDescription() {
+        return hero.getName() + " equips " + equipment.getName();
+    }
+}
