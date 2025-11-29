d5786c3588d3fed30660b3dba2eb09acd7b2536a
akash <akash.sivanandan@pricelabs.co>
2025-11-29 17:34:09 -0500

    Update Game to use a Party object instead of List<Hero>

--- src/core/GameContext.java
+++ src/core/GameContext.java
@@ -1,16 +1,17 @@
 package core;
 
+import party.Party;
 import world.World;
 import character.Hero;
 import java.util.List;
 
 public class GameContext {
     private final World world;
-    private final List<Hero> party;
+    private final Party party;
     private final Renderer renderer;
     private final InputHandler input;
 
-    public GameContext(World world, List<Hero> party, Renderer renderer, InputHandler input) {
+    public GameContext(World world, Party party, Renderer renderer, InputHandler input) {
         this.world = world;
         this.party = party;
         this.renderer = renderer;
@@ -18,7 +19,7 @@ public class GameContext {
     }
 
     public World world()      { return world; }
-    public List<Hero> party() { return party; }
+    public Party party() { return party; }
     public Renderer ui()      { return renderer; }
     public InputHandler in()  { return input; }
 }
