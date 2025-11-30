25ee8fef5e4ff4451b1cb9af65776605d313393b
akash <akash.sivanandan@pricelabs.co>
2025-11-29 20:45:19 -0500

    Add a getter to battleaction

--- src/battle/BattleAction.java
+++ src/battle/BattleAction.java
@@ -1,6 +1,9 @@
 package battle;
 
+import character.Hero;
+
 public interface BattleAction {
     public void execute(Battle battle);
     public String getDescription();
+    public Hero getAttacker();
 }
