87e84daa99f95919c70c13ff74715ea02a2322e4
akash <akash.sivanandan@pricelabs.co>
2025-11-29 20:46:08 -0500

    Add method to create a party in hero list

--- src/factory/HeroFactory.java
+++ src/factory/HeroFactory.java
@@ -1,8 +1,32 @@
 package factory;
 
 import character.*;
+import core.InputHandler;
+import core.Renderer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HeroFactory {
+
+    public static List<Hero> createParty(List<Hero> baseHeroes, InputHandler in, Renderer ui) {
+
+        List<Hero> party = new ArrayList<>();
+
+        while (party.size() < 3) {
+
+            ui.msg("\nChoose hero (or -1 to start):");
+            for (int i = 0; i < baseHeroes.size(); i++)
+                ui.msg(i + ": " + baseHeroes.get(i).getName() + baseHeroes.get(i).getFavouredStats());
+
+            int c = in.nextInt();
+            if (c == -1 && !party.isEmpty()) break;
+            if (c < 0 || c >= baseHeroes.size()) continue;
+
+            party.add(baseHeroes.get(c).copy());
+        }
+        return party;
+    }
 
     public Hero createWarrior(String line) {
         String[] d = line.trim().split("\\s+");
